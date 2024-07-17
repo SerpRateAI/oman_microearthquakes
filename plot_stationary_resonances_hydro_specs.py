abe7ed6d2f5f9932d120d28389825dcf83999d1a
tianzeliu <tianzeliu@gmail.com>
2024-07-17 21:39:16 +0200

    Created the new branch

--- plot_stationary_resonances_hydro_specs.py
+++ plot_stationary_resonances_hydro_specs.py
@@ -12,22 +12,30 @@ from utils_plot import add_colorbar, plot_hydro_stft_spectrograms, save_figure
 # Inputs
 # Data
 station_to_plot = "B00"
-name1 = "SR38a"
+name1 = "MH12a"
 name2 = "SR25a"
+name3 = "SR38a"
 
 window_length = 60.0
 overlap = 0.0
 downsample = False
 downsample_factor = 60
 
-min_freq1 = 38.0
-max_freq1 = 39.0
+min_freq1 = 12.25
+max_freq1 = 13.25
 
 min_freq2 = 25.0
 max_freq2 = 26.0
 
+min_freq3 = 37.75
+max_freq3 = 38.75
+
+pred_freq1 = 12.75
+pred_freq2 = 25.5
+pred_freq3 = 38.25
+
 min_db = -80.0
-max_db = -50.0
+max_db = -60.0
 
 major_time_spacing = "24h"
 num_minor_time_ticks = 4
@@ -36,7 +44,7 @@ major_freq_spacing = 0.5
 num_minor_freq_ticks = 5
 
 # Plotting
-column_width = 8.0
+column_width = 10.0
 row_height = 2.0
 
 colorbar_width = 0.01
@@ -65,11 +73,20 @@ inpath = join(indir, filename)
 hydro_spec2 = read_hydro_spectrograms(inpath,
                                       starttime = starttime, endtime = endtime,
                                       min_freq = min_freq2, max_freq = max_freq2)
+# Signal 3
+suffix = get_spectrogram_file_suffix(window_length, overlap, downsample)
+filename = f"whole_deployment_daily_hydro_spectrograms_{station_to_plot}_{suffix}.h5"
+inpath = join(indir, filename)
+
+hydro_spec3 = read_hydro_spectrograms(inpath,
+                                        starttime = starttime, endtime = endtime,
+                                        min_freq = min_freq3, max_freq = max_freq3)
 
 # Plotting
 print("Plotting the spectrograms...")
 num_loc = len(hydro_spec1)
-fig, axes = subplots(num_loc, 2, figsize=(column_width * 2, row_height * num_loc), sharex = True)
+fig, axes = subplots(num_loc, 3, figsize=(column_width * 2, row_height * num_loc), sharex = True)
+
 # Plot Signal 1
 axes1 = axes[:, 0]
 axes1, _ = plot_hydro_stft_spectrograms(hydro_spec1, 
@@ -79,6 +96,9 @@ axes1, _ = plot_hydro_stft_spectrograms(hydro_spec1,
                                        major_freq_spacing = major_freq_spacing, num_minor_freq_ticks = num_minor_freq_ticks,
                                        location_label_x = location_label_x, location_label_y = location_label_y,
                                        title = name1)
+
+for ax in axes1:
+    ax.axhline(y = pred_freq1, color = "aqua", linestyle = ":", linewidth = 1.0)
 
 
 # Plot Signal 2
@@ -90,19 +110,32 @@ axes2, quadmesh = plot_hydro_stft_spectrograms(hydro_spec2,
                                        major_freq_spacing = major_freq_spacing, num_minor_freq_ticks = num_minor_freq_ticks,
                                        location_label_x = location_label_x, location_label_y = location_label_y,
                                        title = name2)
-
-# Turn off the y labels of axes2
 for ax in axes2:
+    ax.axhline(y = pred_freq2, color = "aqua", linestyle = ":", linewidth = 1.0)
+    ax.set_ylabel("")
+
+# Plot Signal 3
+axes3 = axes[:, 2]
+axes3, _ = plot_hydro_stft_spectrograms(hydro_spec3, 
+                                       axes = axes3,
+                                       dbmin = min_db, dbmax = max_db,
+                                       major_time_spacing = major_time_spacing, num_minor_time_ticks = num_minor_time_ticks,
+                                       major_freq_spacing = major_freq_spacing, num_minor_freq_ticks = num_minor_freq_ticks,
+                                       location_label_x = location_label_x, location_label_y = location_label_y,
+                                       title = name3)
+
+for ax in axes3:
+    ax.axhline(y = pred_freq3, color = "aqua", linestyle = ":", linewidth = 1.0)
     ax.set_ylabel("")
 
 # Add colorbar
-ax = axes2[-1]
+ax = axes3[-1]
 bbox = ax.get_position()
 position = [bbox.x1 + colorbar_gap, bbox.y0, colorbar_width, bbox.height]
 add_colorbar(fig, quadmesh, colorbar_label, position, orientation = "vertical")
 
 # Save the figure
-figname = f"stationary_resonances_hydro_specs_{name1}_{name2}_{station_to_plot}.png"
+figname = f"stationary_resonances_hydro_specs_{name1}_{name2}_{name3}_{station_to_plot}.png"
 save_figure(fig, figname)
 
 
